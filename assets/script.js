f79a3145102c542c92b9e368775c4ec25af2c689
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-05-29 19:37:47 -0500

    adds basic JavaScript for issue 4

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,18 @@
 // DEPENDENCIES
+var searchCity = document.querySelector("#search-input")
+var searchBtn = document.querySelector("#search-button")
 
 // DATA / STATE
 
 // FUNCTIONS
 
 // USER INTERACTIONS
+searchBtn.addEventListener("click", function(event){
+    event.preventDefault;
+    var cityWeather = {
+        name: searchCity.value,
+    }
+    console.log(cityWeather)
+})
 
 // INITIALIZATIONS
