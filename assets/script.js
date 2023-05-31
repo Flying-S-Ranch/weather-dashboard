0d9d7734ab8c12e0c0ca69edf13d5c53676c2b29
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-05-30 19:27:35 -0500

    creates basic button for previously searched items

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,39 @@
 // DEPENDENCIES
 var searchCity = document.querySelector("#search-input")
-var searchBtn = document.querySelector("#search-button")
+console.log(searchCity.value)
+var searchBtn = $("#search-button")
+var searchList = $("#city-search-list")
 
 // DATA / STATE
+var cityWeather;
+// var cityList = [];
 
 // FUNCTIONS
 
+function makeCityBtn() {
+   // create a button with the name of the city
+   var savedCity = JSON.parse(localStorage.getItem("localWeather"))
+   console.log(savedCity.name)
+   var cityBtn = document.createElement("button");
+   // build the button's appearance
+   cityBtn.textContent = savedCity.name;
+   // place the button under the search bar
+   searchList.append(cityBtn)
+
+// for ( i = 0; i < cityList.length, i++)
+
+}
+
 // USER INTERACTIONS
-searchBtn.addEventListener("click", function(event){
+searchBtn.on("click", function(event){
     event.preventDefault;
-    var cityWeather = {
+    cityWeather = {
         name: searchCity.value,
     }
-    localStorage.setItem("Local Weather", JSON.stringify(cityWeather))
+    console.log(searchCity.value)
+    localStorage.setItem("localWeather", JSON.stringify(cityWeather))
     searchCity.value = " "
-})
+    makeCityBtn();
+    })
 
 // INITIALIZATIONS
